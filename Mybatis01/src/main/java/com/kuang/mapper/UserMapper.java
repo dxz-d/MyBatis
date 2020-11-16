4f724fdc6552192e01a3fafceb9168fb48bf22a9
dxz-d <1205760997@qq.com>
2020-11-16 17:51:48 +0800

    mybatis增删改查

--- Mybatis01/src/main/java/com/kuang/mapper/UserMapper.java
+++ Mybatis01/src/main/java/com/kuang/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.kuang.pojo.User;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 /***
  * @description
@@ -15,9 +16,60 @@ import java.util.List;
 public interface UserMapper {
 
     /**
-     * 查询用户列表
+     * 查询全部用户
      *
      * @return
      */
     List<User> getUserList();
+
+    /**
+     * 根据id查询用户
+     *
+     * @param id
+     * @return
+     */
+    User selectUserById(int id);
+
+    /**
+     * 通过密码和名字查询用户
+     * 思路一：直接在方法中传递参数
+     *
+     * @param name
+     * @param pwd
+     * @return
+     */
+    User selectUserByNameAndPassword(@Param("name") String name, @Param("pwd") String pwd);
+
+    /**
+     * 通过密码和名字查询用户
+     * 思路二：使用万能的map
+     *
+     * @param map
+     * @return
+     */
+    User selectUserByNameAndPasswordMap(Map<String, Object> map);
+
+    /**
+     * 给数据库增加一个用户
+     *
+     * @param user
+     * @return
+     */
+    int addUser(User user);
+
+    /**
+     * 修改用户信息
+     *
+     * @param user
+     * @return
+     */
+    int updateUser(User user);
+
+    /**
+     * 根据id删除用户
+     *
+     * @param id
+     * @return
+     */
+    int deleteUser(int id);
 }
--- Mybatis01/src/test/java/com/kuang/mapper/UserMapperTest.java
+++ Mybatis01/src/test/java/com/kuang/mapper/UserMapperTest.java
@@ -5,13 +5,16 @@ import com.kuang.utils.MybatisUntils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 测试类
  */
 public class UserMapperTest {
 
+    // 1.查询用户
     @Test
     public void test() {
 
@@ -31,7 +34,85 @@ public class UserMapperTest {
 
         // 关闭SqlSession
         sqlSession.close();
+    }
 
+    // 2.根据id查询用户
+    @Test
+    public void selectUserById() {
+        // 获取sqlSession连接
+        final SqlSession sqlSession = MybatisUntils.getSession();
+        final UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        final User user = userMapper.selectUserById(1);
 
+        System.out.println(user);
+
+        sqlSession.close();
+    }
+
+    // 通过密码和名字查询用户(方式一：通过参数传递)
+    @Test
+    public void selectUserByNameAndPassword() {
+        final SqlSession sqlSession = MybatisUntils.getSession();
+        final UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        final User user = userMapper.selectUserByNameAndPassword("张三", "abcdef");
+
+        System.out.println(user);
+
+        sqlSession.close();
+    }
+
+    // 通过密码和名字查询用户(方式二：Map传值)
+    @Test
+    public void selectUserByNameAndPasswordMap() {
+        final SqlSession sqlSession = MybatisUntils.getSession();
+        final UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("name", "狂神");
+        map.put("pwd", "123456");
+        final User user = userMapper.selectUserByNameAndPasswordMap(map);
+
+        System.out.println(user);
+
+        sqlSession.close();
+    }
+
+    // 给数据库增加一个用户
+    @Test
+    public void addUser() {
+        final SqlSession sqlSession = MybatisUntils.getSession();
+        final UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        final User user = new User(4, "张聪辉", "666");
+        final int i = userMapper.addUser(user);
+
+        System.out.println("给数据库增加一个用户:" + i);
+
+        // 提交事务，重点！不写的话不会提交到数据库
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    // 修改用户信息
+    @Test
+    public void updataUser() {
+        final SqlSession sqlSession = MybatisUntils.getSession();
+        final UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        final User user = userMapper.selectUserById(4);
+        user.setName("张聪辉name");
+        user.setPwd("6666666");
+        final int i = userMapper.updateUser(user);
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    // 删除用户信息
+    @Test
+    public void deleteUser() {
+        final SqlSession sqlSession = MybatisUntils.getSession();
+        final UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        final int i = userMapper.deleteUser(0);
+
+        sqlSession.commit();
+        sqlSession.close();
     }
 }
